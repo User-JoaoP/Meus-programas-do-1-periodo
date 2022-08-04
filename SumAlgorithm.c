c930b34f75c00a053c20ff8870eaeb60a850c372
fabioantoniio <fabioantonio757@gmail.com>
2022-08-03 22:06:07 -0300

    Sum Algorithm

---
+++ SumAlgorithm.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <locale.h>
+    
+int main(){
+
+    int number, sum;
+	
+	printf("Algorithm to calculate the sum of the first n positive integers..\n");
+    printf("----------------------------------------------------------------\n");
+
+	printf("Enter the value of n:\n");
+	scanf("%d", &number);
+	sum = 0;
+	int i = 1;
+	
+	while (i <= number){
+		sum += i;
+		i += 1;
+	}  
+	printf("The sum of the first %d positive integers is %d.\n", number, sum);
+	return 0;
+}
